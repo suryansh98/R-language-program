e0ae6d69d39e179ad5e9873a4b042c86bde29a84
suryansh98 <suryansh.official100@gmail.com>
2019-08-28 11:36:36 +0530

    Add files via upload

--- linear regression.r
+++ linear regression.r
@@ -1,5 +1,9 @@
 toyotac<-read.csv("-/toyotac.csv")
 View(toyotac)
 #ggplot(toyotac,aes(y=Price,x=Age))+geom_point()
-ggplot(toyotac,aes(y=Price,x=KM))+geom_point()
+#ggplot(toyotac,aes(y=Price,x=KM))+geom_point()
+split=sample.split(toyotac$Price,SplitRatio = 0.8)
+training_set=subset(toyotac,split==TRUE)
+
+test_set=subset(toyotac,split==FALSE)
 
